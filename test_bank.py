f77c63cb49de2bd6c8fe2d65c33ce3772f9ff4a4
joshuaabrookuk <joshuabrookuk@gmail.com>
2022-03-03 16:30:30 +0000

    Refactor BankTests, removing duplicate code in favor of the setUp method

--- test_bank.py
+++ test_bank.py
@@ -3,14 +3,15 @@ from bank import Bank
 
 class BankTests(unittest.TestCase):
 
+    def setUp(self):
+        self.bank = Bank()
+
     def test_bank_banance(self):
-        bank = Bank()
-        self.assertEqual(bank.balance, 0)
+        self.assertEqual(self.bank.balance, 0)
 
     def test_bank_deposit(self):
-        bank = Bank()
-        bank.credit(1000)
-        self.assertEqual(bank.balance, 1000)
+        self.bank.credit(1000)
+        self.assertEqual(self.bank.balance, 1000)
 
 if __name__ == '__main__':
     unittest.main()
